5db606e2b0094637355e978ddadd75ca44427962
harshit gupta <harshit.28.sps@gmail.com>
2022-01-22 21:00:45 +0530

    IPA-Xplore

---
+++ 02_IPA 05-Dec-2021/Question-1/solution.py
@@ -0,0 +1,28 @@
+def isPresent(lis,st):
+    for i in range(0, len(lis)):
+        if lis[i] == st:
+            return i
+
+lis = []
+for j in range(int(input())):
+    lis.append(input())
+st = input()
+
+ind = isPresent(lis,st)
+
+if ind == -1:
+    print("String not found")
+else:
+    print("Position of the searched string is: ",ind)
+
+
+# Input:
+# 4
+# Hello Good Morning
+# abcd123Fghy
+# India
+# Progoti.c
+# India
+
+# Output:
+# Position of the searched string is: 2
